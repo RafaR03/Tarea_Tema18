29ab8b0675e350dc215776b9af8aa402fc52f869
RafaR03 <210770c@gmail.com>
2023-04-14 19:55:52 +0200

    Cambio 4

--- src/Funciones.java
+++ src/Funciones.java
@@ -42,9 +42,7 @@ public class Funciones{
    * @return El valor de la variable div.
    */
     public static int div( int x,int q){
-        int div=0;
-        div=x/q;
-        return div;
+        return x/q;
     }
 /**
  * La función resto() toma dos enteros como entrada y devuelve el resto del primer entero dividido por
